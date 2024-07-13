0b1a48ba1529604e00e91c5a8d2187d9abe9f071
Tyrone Faulhaber <20131658+spectrapulse@users.noreply.github.com>
2024-07-13 17:23:22 +0200

    Changed maximum brightness to 50%

--- src/main/java/com/laidbacksloth/brightnessslider/BetterBrightnessSlider.java
+++ src/main/java/com/laidbacksloth/brightnessslider/BetterBrightnessSlider.java
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod("brightnessslider")
 public class BetterBrightnessSlider {
-    public static double min = -1;
-    public static double max = 12;
+    public static double min = 0;
+    public static double max = .5;
     public static double interval = 0.05;
 }
